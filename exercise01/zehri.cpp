a93e27896afe1e2a81c8025e12b1c14d29e66b75
Ahmad Yoosofan <yoosofan@gmx.com>
2021-02-27 16:37:27 +0330

    zekri

---
+++ exercise01/zehri.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+void searchph();
+void extra();
+int manu();
+
+struct Phone
+{
+    char name[20];
+    char family[20];
+    int number;
+};
+
+struct Phone people[100];
+
+int main()
+{
+    menu();
+    return 0;
+}
+
+int menu()
+{
+    int a;
+    printf("1_add phone number\n2_search phone number\n");
+    printf("which one menu:");
+    scanf("%d",&a);
+    switch(a)
+    {
+      case 1:
+        extra();
+        break;
+      case 2:
+        searchph();
+        break;
+    }
+}
+
+void extra()
+{
+    int b,i,s;
+    printf("how much phone number:");
+    scanf("\n%d",&b);
+    for( i=0 ; i<b ; i++)
+    {
+        printf("enter name:");
+        scanf("%s",people[i].name);
+        printf("enter family:");
+        scanf("%s",people[i].family);
+        printf("enter phone number:");
+        scanf("%d",&people[i].number);
+        printf("information saved\n");
+    }
+    menu();
+}
+void searchph(){
+    int j,n;
+    printf("enter phone number for search:");
+    scanf("%d",&j);
+    for( n=0 ; n<100 ; n++)
+    {
+        if( j==people[n].number)
+        {
+            printf("name=%s\n",people[n].name);
+            printf("family=%s",people[n].family);
+        }
+    }
+}

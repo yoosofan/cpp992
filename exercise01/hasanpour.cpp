b77f010ecf6d0aaf53a866cffa8bd9564660dc0a
Ahmad Yoosofan <yoosofan@gmx.com>
2021-03-01 16:30:40 +0330

    first cin cout

--- exercise01/hasanpour.cpp
+++ exercise01/hasanpour.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
 struct information
 {
     char firstName[20];
@@ -8,19 +9,23 @@ struct information
     char phoneNum[12];
 
 }info;
-void Search();
-void add();
+
+void Search(void);
+void add(void);
+
 struct infoArr
 {
-     struct information arr[100];
+     information arr[100];
      int t;
 
 }infoArr;
 
 int main()
 {
-    printf("1-Adding new person number\n2-Search a telephone number\n");
-    int num;scanf("%d",&num);
+    //printf("1-Adding new person number\n2-Search a telephone number\n");
+    cout << "1-Adding new person number\n2-Search a telephone number\n";
+    int num;
+    cin >> num;
     switch(num){
     case 1:
         add();
@@ -29,7 +34,7 @@ int main()
         Searchs();
         break;
     default:
-        printf("Enter a valid number!");
+        cout << "Enter a valid number!";
         break;
     }
     return 0;
@@ -38,7 +43,7 @@ int main()
 void add()
 {
     int i;
-    printf("How many people do u want to add?\n");
+    cout << "How many people do u want to add?\n";
     scanf("%d" ,&infoArr.t);
     int t=infoArr.t;
     for(i=1 ; i<=infoArr.t ; i++)

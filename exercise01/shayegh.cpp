349b607d50bc1ce265f619962143701477942a71
Ahmad Yoosofan <yoosofan@gmx.com>
2021-03-01 15:49:45 +0330

    const int

--- exercise01/shayegh.cpp
+++ exercise01/shayegh.cpp
@@ -4,17 +4,20 @@
 void Search();
 void add();
 
+const int NUMBER_OF_CHAR_IN_NAMES = 20;
+const int NUMBER_OF_STUDENTS_IN_CLASS = 100;
+
 struct student
 {
-    char firstName[20];
-    char lastName[20];
+    char firstName[NUMBER_OF_CHAR_IN_NAMES];
+    char lastName[NUMBER_OF_CHAR_IN_NAMES];
     char phoneNum[12];
 
 }stu;
 
 struct person
 {
-    struct student arr[100];
+    struct student arr[NUMBER_OF_STUDENTS_IN_CLASS];
     int c;
 
 }per;
@@ -38,14 +41,14 @@ void add()
     int b;
     printf("\nChand ta ezafe mikoni?!\n");
     scanf("%d" , &a);
-    for(b=1 ; b<=a ; b++)
+    for(b=0 ; b < a ; b++)
     {
         printf("\nEnter firstName :\n");
-        scanf("%s" , per.arr[100].firstName);
+        scanf("%s" , per.arr[b].firstName);
         printf("\nEnter lastName :\n");
-        scanf("%s" ,per.arr[100].lastName);
+        scanf("%s" ,per.arr[b].lastName);
         printf("\nEnter phoneNum :\n");
-        scanf("%s" , per.arr[100].phoneNum);
+        scanf("%s" , per.arr[b].phoneNum);
     }
 
 }
@@ -53,11 +56,11 @@ void add()
 
 void Search()
 {
-    char lname[20];
-    //int i;
+    char lname[NUMBER_OF_CHAR_IN_NAMES];
+    int i;
     printf("\nSearch for lastName :\n ");
     scanf("%s" , lname);
-    for(i=0 ; i<=100 ; i++)
+    for(i=0 ; i < NUMBER_OF_STUDENTS_IN_CLASS ; i++)
     {
         int x = strcmp(per.arr[i].lastName,lname);
         if(x == 1 || x == 0){

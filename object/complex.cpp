4cdead1512c24ff34a9535251b7c2cba0d1da903
Ahmad Yoosofan <yoosofan@gmx.com>
2021-03-08 15:52:58 +0330

    date: Mon 08 Mar 2021 03:52:58 PM +0330

--- object/complex.cpp
+++ object/complex.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 struct complexCls{
   double re, img;
 }
 
-void 
+complexCls input(void);
+void print(complexCls);
+complexCls negate(complexCls);
+double magnitude(complexCls);
+
+void test(void);
+
+int main(){
+  test();
+  return 0;
+}
+
+complexCls input(void){
+  complexCls a;
+  cout << "Enter real part: ";
+  cin >> a.re;
+  cout << "Enter imaginary part: ";
+  cin >> a.img;
+  return a;
+}
+
+void print(complexCls a){
+  cout << '(' << a.re << ", " << a.img << ')' << endl;
+}
+
+complexCls negate(complexCls a){
+  a.re = - a.re;
+  a.img = - a.img;
+  return a;
+}
+
+double magnitude(complexCls a){
+  return sqrt(a.re * a.re + a.img * a.img);
+}

24f6b83035e30d3f94de65de00782fab7bea5e5d
Ahmad Yoosofan <yoosofan@gmx.com>
2021-03-08 16:16:46 +0330

    date: Mon 08 Mar 2021 04:16:46 PM +0330

--- object/complex02.cpp
+++ object/complex02.cpp
@@ -3,7 +3,7 @@
 // using namespace std;
 
 struct complexCls{
-  double re, img;
+  double re = 0, img = 0;
 
   void input(void){
     std::cout << "Enter real part: ";
@@ -42,6 +42,7 @@ void test(void){
   b.print();
   
   complexCls a;
+  a.print();
   a.input();
   a.print();
   double d = a.magnitude();

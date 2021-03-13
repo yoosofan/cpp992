c5296314a4a03d09c9ed9fbb68c549c13ebbed78
Ahmad Yoosofan <yoosofan@gmx.com>
2021-03-13 16:05:09 +0330

    add complex03

---
+++ object/complex03.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+// using namespace std;
+
+struct complexCls{
+  double re, img;
+
+  complexCls(){
+    re = 0;
+    img = 0;
+  }
+  complexCls(double r){
+    re = r;
+    img = 0;
+  }
+  complexCls(double r, double i){
+    re = r;
+    img = i;
+  }
+  void input(void){
+    std::cout << "Enter real part: ";
+    std::cin >> re;
+    std::cout << "Enter imaginary part: ";
+    std::cin >> img;
+  }
+
+  void print(void){
+    std::cout << '(' << re << ", " << img << ')' << std::endl;
+  }
+
+  void negate(void){
+    re = - re;
+    img = - img;
+  }
+
+  double magnitude(void){
+    return std::sqrt(re * re + img * img);
+  }
+};
+
+void test(void);
+
+int main(){
+  test();
+  return 0;
+}
+
+void test(void){
+  complexCls b(1, 4);
+  b.print();
+  b.input();
+  std::cout << b.magnitude() << std::endl;
+  b.print();
+  b.negate();
+  b.print();
+  
+  complexCls a;
+  a.print();
+  a.input();
+  a.print();
+  double d = a.magnitude();
+  std::cout << d << std::endl;
+  
+  complexCls c(2);
+  c.input();
+  c.print();
+}

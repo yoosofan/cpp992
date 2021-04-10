10d28916555a9c3a267eb31ad4ba2a474aa0ba6d
Ahmad Yoosofan <yoosofan@gmx.com>
2021-04-10 16:21:47 +0430

    several sample

--- object/reference/01.cpp
+++ object/reference/01.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 int main(){
-	int a=0;
-	int&b=a;
-	int c=a;
-	std::cout<<a<<"\t"<<c<<"\t"<<std::endl;
-	b=3;
-	std::cout<<a<<"\t"<<b<<c<<"\t"<<std::endl;
+	int a = 0;
+	int& b = a;
+	int c = a;
+	std::cout << a << "\t" << c << "\t" << std::endl;
+	b = 3;
+	std::cout << a << "\t" << b << c << "\t" << std::endl;
 	return 0;
 }

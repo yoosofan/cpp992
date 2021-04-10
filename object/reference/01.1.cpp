e67e31fdf8f691302f84fbb3eb6653ff9f5a5e13
Ahmad Yoosofan <yoosofan@gmx.com>
2021-04-10 16:33:36 +0430

    several sample

--- object/reference/01.1.cpp
+++ object/reference/01.1.cpp
@@ -4,19 +4,19 @@ int main(){
 	int a = 0;
 	int& b = a;
 	int c = a;
-	std::cout << a << "\t" << c << "\t" << std::endl;
+	std::cout << __LINE__ << ":\t" << a << "\t" << c << "\t" << std::endl;
 	b = 3;
-	std::cout << a << "\t" << b << '\t' << c << "\t" << std::endl;
+	std::cout << __LINE__ << ":\t" << a << "\t" << b << '\t' << c << "\t" << std::endl;
   int *pa = nullptr;
   pa = & a;
-  std:: cout << *pa << std::endl;
+  std:: cout << __LINE__ << ":\t" << *pa << std::endl;
   pa = & c;
   c = 4;
-  std:: cout << *pa << std::endl;
+  std:: cout << __LINE__ << ":\t" << *pa << std::endl;
   b = c;
-	std::cout << a << "\t" << c << "\t" << std::endl;
+	std::cout << __LINE__ << ":\t" << a << "\t" << c << "\t" << std::endl;
   f1(pa);
-  std::cout << a << "\t" << c << "\t" << std::endl;
+  std::cout << __LINE__ << ":\t" << a << "\t" << c << "\t" << std::endl;
 	return 0;
 }
 

ba68f2aa335bac1ffb2c09b8f9731c4bd6aee4f3
Ahmad Yoosofan <yoosofan@gmx.com>
2021-04-10 16:19:29 +0430

    several sample

---
+++ object/reference/03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int& test();
+int main(){
+	cout << test()<<endl;
+    test() = 5;
+    cout << test()<<endl;
+    return 0;
+}
+int& test(){
+	static int num=0;
+    return num;
+}

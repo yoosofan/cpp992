011af2f253c30d4ed50bd68741a1fcdce430646d
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-03 16:01:36 +0430

    t01

--- template.functions/template01.cpp
+++ template.functions/template01.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-template<typename T> int search(T *a, T v, int n){
+template<typename T> int search(T* a, T v, int n){
   for(int i = 0; i < n; i++) 
     if(a[i] == v) 
       return i;
@@ -13,7 +13,8 @@ int main(){
   cout << search<double>(ae, 3, 6) << endl;
   cout << search<double>(ae, 33, 6) << endl;
   string am[] = {"ali", "reza", "kamran", "hamid"};
-  cout << search<string>(am, "reza", 4) << endl;
+  cout << search<string>(am, "reza", 4) << endl; // type of "reza" is 'const char *'
+  // cout << search(am, "ali", 4) << endl;
   cout << search(am, string("ali"), 4) << endl;
   string s3 = "kamran";
   cout << search(am, s3, 4) << endl;

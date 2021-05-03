192400cc129d567a6371ba8d28b823c7faf45759
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-03 16:49:07 +0430

    array04

--- template.more/array04.cpp
+++ template.more/array04.cpp
@@ -1,24 +1,35 @@
 //Error
 #include<iostream>
 using namespace std;
-const int MAX=200;
+
 class complexCls{
   double re,im;
  public:
   complexCls(double a=0,double b=0){re=a;im=b;}
 };
-template<typename Type> class array{Type a[MAX];  int n;
+
+template<typename Type> class array{
+  static const int MAX = 200;
+  Type a[MAX];  
+  int n = 0;
 public:
   array(int n=20){this->n=n;}
   ~array(){n=0;}
   void input(void){cout<<"Enter n: ";cin>>n;
-    for(int i=0;i<n;i++){cout<<"Enter a["<<i<<"]: ";cin>>a[i];}
+    for(int i=0;i<n;i++){
+      cout<<"Enter a["<<i<<"]: ";
+      cin >> a[i];
+    }
   }
   void print(void){cout<<"n: "<<n<<endl;for(int i=0;i<n;i++)cout<<a[i]<<endl;}
 };
 int main(){
-  array<int> m1,m2,m3; array<double> dm;   array<char> cm;
-  array<complexCls> ccm1; ccm1.input();
-  m1.input();m1.print();
+  array<int> m1,m2,m3; 
+  array<double> dm;   
+  array<char> cm;
+  array<complexCls> ccm1; 
+  ccm1.input();
+  m1.input();
+  m1.print();
   return 0;
 }

fdad69b4a730a818d8cd4f746680113acb649ebc
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-03 15:56:44 +0430

    t01

--- template.functions/template01.cpp
+++ template.functions/template01.cpp
@@ -14,5 +14,6 @@ int main(){
   cout << search<double>(ae, 33, 6) << endl;
   string am[] = {"ali", "reza", "kamran", "hamid"};
   cout << search<string>(am, "reza", 4) << endl;
+  cout << search(am, "ali", 4) << endl;
   return 0;
 }

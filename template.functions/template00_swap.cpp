50078c2fb34b690ff5b6038b3f8f723cfb503299
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-03 15:49:57 +0430

    swap00

--- template.functions/template00_swap.cpp
+++ template.functions/template00_swap.cpp
@@ -3,11 +3,12 @@ using namespace std;
 void swap(double&, double&);
 void swap(string&, string&);
 int main(){
-  double ae[20] = {3, 4, 5, 6, 8, 9};
-  cout << search(ae, 3, 6) << endl;
-  cout << search(ae, 33, 6)  << endl;
-  string am[]={"ali", "reza", "kamran", "hamid"};
-  cout << search(am,"reza",4) << endl;
+  double x = 7, y = 87;
+  string s1 = "abc", s2 = "zghm";
+  swap(x, y);
+  cout << "x: " << x << "\t y: " << y << endl;
+  swap(s1, s2);
+  cout << "s1: " << s1 << "\t s2: "<< s2 << endl;
   return 0;
 }
 void swap(double& a, double& b){

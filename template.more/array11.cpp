187a9aa696f4b9754fc3669f3804d8d3cc11dc1e
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-08 16:26:21 +0430

    test

--- template.more/array11.cpp
+++ template.more/array11.cpp
@@ -1,22 +1,68 @@
 #include<iostream>
 using namespace std;
-template<class Type> class array{  Type *a;  int n;  public:
-  array(int n=20)  {this->n=n;a=new Type[n];}
-  ~array(){n=0;delete[] a; a=0;}
-  Type & operator[](int index){  if(index<n && index >= 0) return a[index];
+template<class Type> class array{  
+  Type *a;  
+  int n;  
+  public:
+
+  array(int n=20){
+    this->n = n;
+    a = new Type[n];
+    if(a == nullptr)
+      throw "not sufficient memory for new allocation";
+  }
+
+  ~array(){
+    n = 0;
+    delete[] a; 
+    a =nullptr;
+  }
+
+  Type & operator[](int index){  
+    if(index < n && index >= 0) 
+      return a[index];
+    else
+      throw "Index out of range";
     return a[0];
-  }array operator=(array f){ delete[]a;n=f.n;a=new Type[n];
-    for(int i=0;i<n;i++)a[i]=f.a[i];
+  }
+  
+  array operator =(const array& f){ 
+    delete[] a;
+    n = f.n;
+    a = new Type[n];
+    for(int i=0;i<n;i++)
+      a[i] = f.a[i];
     return *this;
-  } array operator+=(array m1){ *this =m1+*this; return *this;}
-  array(const array &f){n=f.n;a=new Type[n];    for(int i=0;i<n;i++)a[i]=f.a[i];  }
-  array operator+(array m1){array retVal;int i; array *pmax,*pmin;
-    pmax=m1.n>this->n? &m1:this;     pmin=m1.n<=this->n?&m1:this;
-    retVal.n=pmax->n;     retVal.a=new Type[retVal.n];
-    for(i=0;i<pmin->n;i++)     retVal.a[i]=pmin->a[i]+pmax->a[i];
-    for(;i<pmax->n;i++)        retVal.a[i]=pmax->a[i];
+  } 
+  
+  array operator +=(array m1){ 
+    *this = m1 + *this; 
+    return *this;
+  }
+
+  array(const array &f){
+    n = f.n;
+    a = new Type[n];    
+    for(int i=0;i<n;i++)
+      a[i]=f.a[i];  
+  }
+
+  array operator +(const array& m1){
+    array retVal;
+    int i; 
+    array *pmax,*pmin;
+    pmax = m1.n > this->n ? &m1:this;
+    pmin = m1.n <= this->n ? &m1:this;
+    retVal.n = pmax->n;     
+    retVal.a = new Type[retVal.n];
+    for(i=0; i < pmin->n; i++)     
+      retVal.a[i] = pmin->a[i] + pmax->a[i];
+    for(;i<pmax->n;i++)        
+      retVal.a[i] = pmax->a[i];
     return retVal;
-  } template<class T> friend array<T> operator-(array<T>&, array<T>&);
+  } 
+  
+  template<class T> friend array<T> operator-(array<T>&, array<T>&);
   template<class T>friend ostream& operator<<(ostream&o1,array<T> m1);
 }; template<class T> array<T> operator-(array<T>&m1,array<T>&m2){array<T> retVal;int i;
     array<T> *pmax,*pmin; pmax=m1.n>m2.n? &m1:&m2; pmin=m1.n<=m2.n?&m1:&m2;

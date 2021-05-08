267a1a87412127ec602393a482362121043d56d7
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-08 16:36:35 +0430

    test

--- template.more/array11.cpp
+++ template.more/array11.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-template<class Type> class array{  
+template<typename Type> class array{  
   Type *a;  
   int n;  
   public:
@@ -61,19 +61,44 @@ template<class Type> class array{
       retVal.a[i] = pmax->a[i];
     return retVal;
   } 
-  
-  template<class T> friend array<T> operator-(array<T>&, array<T>&);
-  template<class T>friend ostream& operator<<(ostream&o1,array<T> m1);
-}; template<class T> array<T> operator-(array<T>&m1,array<T>&m2){array<T> retVal;int i;
-    array<T> *pmax,*pmin; pmax=m1.n>m2.n? &m1:&m2; pmin=m1.n<=m2.n?&m1:&m2;
-    retVal.n=pmax->n; retVal.a=new T[retVal.n];
-    for(i=0;i<pmin->n;i++)    retVal.a[i]=pmin->a[i]-pmax->a[i];
-    for(;i<pmax->n;i++)       retVal.a[i]=pmax->a[i];
+
+  template<typename T> friend array<T> operator-(const array<T>&m1, const array<T>&m2){
+    array<T> retVal;
+    int i;
+    array<T> *pmax,*pmin; 
+    pmax = m1.n > m2.n ? &m1:&m2; 
+    pmin = m1.n <= m2.n ? &m1:&m2;
+    retVal.n = pmax->n; 
+    retVal.a = new T[retVal.n];
+    for(i=0;i<pmin->n;i++)    
+      retVal.a[i] = pmin->a[i] - pmax->a[i];
+    for(;i<pmax->n;i++)       
+      retVal.a[i]=pmax->a[i];
     return retVal;
-} template<class T> ostream& operator<<(ostream&o1,array<T> m1)
-{o1<<m1.n<<endl;  for(int i=0;i<m1.n;i++)    o1<<m1.a[i]<<endl;  return o1;}
-int main(){array<int> m1,m2,m3; array<double> dm;   array<char> cm;  cout<<m1;
-  m3=m2=m1;   cout<<m1;  //array<int> m4(m1);  array<int>m5=m1;
-  m1=m2+m3; cout<<m1;   m1=m1+m3; cout<<m1;  m1=m1-m2; cout<<m1;
-  m1+=m2;   cout<<m1;  return 0;
+  }  
+
+  template<typename T> friend ostream& operator<<(ostream&o1,array<T> m1){
+    o1<<m1.n<<endl;  
+    for(int i=0;i<m1.n;i++)    
+      o1<<m1.a[i]<<endl;  
+    return o1;
+  }
+}; 
+
+int main(){
+  array<int> m1, m2, m3; 
+  array<double> dm;   
+  array<char> cm;  
+  cout << m1;
+  m3 = m2 = m1;   
+  cout<<m1;  //array<int> m4(m1);  array<int>m5=m1;
+  m1 = m2 + m3; 
+  cout << m1;   
+  m1 = m1 + m3; 
+  cout << m1;  
+  m1 = m1 - m2; 
+  cout<<m1;
+  m1 += m2;   
+  cout<<m1;  
+  return 0;
 }

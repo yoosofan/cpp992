b9b7e2ae2a2766a4790f69a7cd631902f6ac562e
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-22 15:51:51 +0430

    inheritance

--- inheritance/inheritence1.cpp
+++ inheritance/inheritence1.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 using namespace std;
+
 class Shape{
   string name;// char name[100]; '\0' رشته
   string color;// strcpy, strcmp; char*color; str1=str2
-public:
+ public:
   int Area(void){return 0;}
 };
+
 class Square: public Shape{
   int d;
 public:
-  int Area(void){return d*d;}
-  Square(int d=1){this->d=d;}
+  Square(int d=1){
+    this->d = d;
+  }
+  int Area(void){
+    return d*d;
+  }
+
 };
 int main(){
   Shape s;

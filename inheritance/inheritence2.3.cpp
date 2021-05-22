ce5436053be3ffcdb6281ab578fb20707877e4d7
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-22 16:29:33 +0430

    inheritance

--- inheritance/inheritence2.3.cpp
+++ inheritance/inheritence2.3.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
+
 class A{};
+
 class Shape: public A{
 protected:
   string name;
@@ -8,19 +10,23 @@ protected:
 public:
   int Area(void){return 0;}
 };
+
 class Square: public Shape{
   int d;
 public:
   int Area(void){return d*d;}
   Square(int d=1){this->d=d;}
 };
+
 int main(){
   Shape s;
   cout<<s.Area()<<endl;
   Square sq(3);
   cout<<sq.Area()<<endl;
-  Shape *ps=&s; cout<<ps->Area()<<endl;
-  ps=&sq; cout<<ps->Area()<<endl;
+  Shape *ps=&s; 
+  cout<<ps->Area()<<endl;
+  ps=&sq; 
+  cout<<ps->Area()<<endl;
   A *pa;
   return 0;
 }

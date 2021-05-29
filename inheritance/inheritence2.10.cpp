7bf37b35e0a76d38b97279bead0b533db8fb93cf
Ahmad Yoosofan <yoosofan@gmx.com>
2021-05-29 16:18:59 +0430

    test

--- inheritance/inheritence2.10.cpp
+++ inheritance/inheritence2.10.cpp
@@ -44,15 +44,20 @@ void f1(Shape a){
   cout << a.Area() << endl;
   a.show();
 }
+void f2(Square s1){
+  cout << s1.Area() << endl;
+}
 
 int main(){
   Shape s;
   f1(s);
-  Square sq(3); // f1(sq);
+  f2(s);
+  Square sq(3); 
+  f1(sq);
   drive1 d1; 
   d1.f(sq);
   //cout<<sq.Area()<<endl;
-  Shape* ps=&s; 
+  Shape* ps = &s; 
   cout << ps->Area() << endl;
   //ps=&sq; cout<<ps->Area()<<endl;
   return 0;
